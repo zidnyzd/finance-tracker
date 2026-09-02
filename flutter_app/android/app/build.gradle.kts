0797f0fd311b17e194827d6448b7188abe79a3c8
zidnyzd <76208286+zidnyzd@users.noreply.github.com>
2026-09-02 20:52:58 +0700

    feat(ci): v2.0.1 generate split-debug-info and native obfuscation symbols in App Bundle for Google Play Console

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "id.web.zira.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 60
-        versionName = "2.0.0"
+        versionCode = 61
+        versionName = "2.0.1"
         signingConfig = signingConfigs.getByName("release")
     }
 
