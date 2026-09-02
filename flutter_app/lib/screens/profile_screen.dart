1c6fb5fae6455ed7e510f2b3314b430c85aadde7
zidnyzd <76208286+zidnyzd@users.noreply.github.com>
2026-09-02 19:04:02 +0700

    fix(flutter): import TablerIcons in ProfileScreen for Instant Notification Tester button

--- flutter_app/lib/screens/profile_screen.dart
+++ flutter_app/lib/screens/profile_screen.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:tabler_icons/tabler_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/models.dart';
 import '../providers/app_provider.dart';
