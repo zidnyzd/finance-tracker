a7987afc5f833c69f1aa46eaa8c0280a097e34a4
zidnyzd <76208286+zidnyzd@users.noreply.github.com>
2026-09-02 07:26:03 +0700

    fix(flutter): add dart:convert, dart:io, http, and path_provider imports to profile_screen

--- flutter_app/lib/screens/profile_screen.dart
+++ flutter_app/lib/screens/profile_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/models.dart';
